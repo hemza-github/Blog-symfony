83378fa33e5beed331fe0a6811878734b00f2f1e
hemza <hemza.elmiri@gmail.com>
2020-04-10 00:02:15 +0200

    uptdate

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,18 +2,34 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class BlogController extends AbstractController
 {
     /**
      * @Route("/blog", name="blog")
      */
-    public function blog()
+    public function blog(ArticleRepository $articleRepository)
     {
+
+        $articles = $articleRepository-> findAll();
         return $this->render('blog/blog.html.twig', [
             'controller_name' => 'BlogController',
+            'articles' => $articles,
         ]);
+    }
+    
+
+     /**
+     * @route("/blog/{id}/view", name="blog_view")
+     */
+    public fonction view(Article $article){
+        return $this->render('blog/view.html.twig', [
+            'controller_name' => 'bienvenue sur l\'article', 
+
+
     }
 }
